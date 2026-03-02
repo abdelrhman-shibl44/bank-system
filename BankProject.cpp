dd9085ef5902fcc57753ca0673d771689759aea1
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2026-03-02 11:10:16 +0200

    Implement client data management features: find, delete, and save clients

--- BankProject.cpp
+++ BankProject.cpp
@@ -55,11 +55,9 @@ void saveLineToFile(string line){
     }
 }
 
-
 void header(string headerTxt){
     cout << "====================================================\n";
     cout << "=================" << headerTxt << "================\n";
-    cout << "====================================================\n";
 }
 
 void addNewClient(){
@@ -70,10 +68,122 @@ void addNewClient(){
     cout << "Client has been added successfully\n";
 }
 
+stClientData convertLineToRecord(string line){
+    stClientData clientData;
+    vector<string> data = split(line, "#//#");
+    clientData.accountNumber = data[0];
+    clientData.pinCode = data[1];
+    clientData.name = data[2];
+    clientData.phone = data[3];
+    clientData.Balance = stod(data[4]);
+    return clientData;
+}
+
+vector<stClientData> readDataFromFile(string fileName){
+    fstream file;
+    vector<stClientData> vClients;
+    file.open(fileName, ios::in);
+    if(file.is_open()){
+        string line;
+        while(getline(file, line)){
+            stClientData client =  convertLineToRecord(line);
+            vClients.push_back(client);
+        }
+        file.close();
+    }
+    return vClients;
+}
+
+void printClientData(stClientData client){
+    cout << "| " << left << setw(20) <<  client.accountNumber;
+    cout << "| " << left << setw(10) <<  client.pinCode;
+    cout << "| " << left << setw(35) <<  client.name;
+    cout << "| " << left << setw(15) <<  client.phone;
+    cout << "| " << left << setw(15) <<  client.Balance;   
+}
+
+void printClientsData(){
+    header("Show Client Data");
+    vector<stClientData> vClients = readDataFromFile(fileName);
+        cout << "| " << left << setw(20) << "Account Number";
+        cout << "| " << left << setw(10) << "PinCode";
+        cout << "| " << left << setw(35) << "Name";
+        cout << "| " << left << setw(15) << "Phone";
+        cout << "| " << left << setw(15) << "Balance";
+        cout << "\n--------------------------------------------------------------------------------------------------\n";
+    for(stClientData &client: vClients){
+        printClientData(client);
+        cout << endl;
+    }
+}
+
+bool findClient(string accountNumber = readString("please Enter Account number you're looking for? "), stClientData client = {}){
+    vector<stClientData> vClients = readDataFromFile(fileName);
+    for(stClientData &client: vClients){
+        if(convertTextToLower(client.accountNumber) == convertTextToLower(accountNumber)){
+            header("Client Data");
+            printClientData(client);
+            cout << endl;
+            client = client;
+            return true;
+        }
+    }
+    return false;
+
+}
+
+void saveClientsToFile(vector<stClientData> vClients){
+    fstream file; 
+    file.open(fileName, ios::out);
+    if(file.is_open()){
+        for(stClientData &client: vClients){
+            if(!client.markforDeletion){
+                string line = convertRecordToLine(client);
+                file << line << endl;
+            }
+        }
+        file.close();
+    }
+}
+
+void markClientForDeletion(string accountNumber, vector<stClientData> &vClients){
+    for(stClientData &client: vClients){
+        if(convertTextToLower(client.accountNumber) == convertTextToLower(accountNumber)){
+            client.markforDeletion = true;
+        }
+    }
+}
+
+void deleteClient(){
+    string accountNumber = readString("please Enter Account number you want to delete? ");
+    vector<stClientData> vClients = readDataFromFile(fileName); 
+    stClientData client;
+    if(findClient(accountNumber, client)){
+        char answer = readChar("Are you sure you want to delete this account? \n");
+        if(tolower(answer) == 'y'){
+            markClientForDeletion(accountNumber,vClients);
+            saveClientsToFile(vClients);
+            cout << "Account Deleted successfully :)";
+            vClients = readDataFromFile(fileName);
+        }
+    }else{
+        cout << "The Account you're looking for not exist or already deleted";
+    }
+}
+
 short chooseFromMenu(short number){
     switch ((enOptions) number){
+        case enOptions::SHOW: 
+            printClientsData();
+            break;  
         case enOptions::ADD:
             addNewClient();
+            break;
+        case enOptions::FIND:
+            findClient();
+            break;
+        case enOptions::DELETE:
+            deleteClient();
             break;
     
         default:
